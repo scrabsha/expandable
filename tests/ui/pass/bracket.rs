ba4f17154f37a6c4e4b9e553f9beebaa931b53ce
Sasha Pourcelot <sasha.pourcelot@protonmail.com>
2023-10-30 23:17:48 +0100

    Add [<expr>; <expr>] frontend test
    Signed-off-by: Sasha Pourcelot <sasha.pourcelot@protonmail.com>

--- tests/ui/pass/bracket.rs
+++ tests/ui/pass/bracket.rs
@@ -4,6 +4,10 @@ macro_rules! mk_array {
     ( $( $e:expr ),* $(,)? ) => {
         [ $( $e ),* ]
     };
+
+    ( $repeat:expr; $size:expr ) => {
+        [ $repeat; $size ]
+    };
 }
 
 fn main() {}
