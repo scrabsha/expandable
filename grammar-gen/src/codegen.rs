573c0ee206713ae46a4909a3f1e89863c373df5f
Sasha Pourcelot <sasha.pourcelot@protonmail.com>
2024-04-04 13:55:08 +0200

    Check that all the functions called are defined
    Signed-off-by: Sasha Pourcelot <sasha.pourcelot@protonmail.com>

--- grammar-gen/src/codegen.rs
+++ grammar-gen/src/codegen.rs
@@ -1,14 +1,19 @@
 pub(crate) mod rt;
 
-use proc_macro2::TokenStream;
+use std::collections::HashSet;
 
-use crate::mir::{Production, Vis};
+use proc_macro2::TokenStream;
+use syn::Ident;
+
+use crate::mir::{Production, ProductionKind, Vis};
 
 pub(crate) fn generate_code(productions: Vec<Production>) -> TokenStream {
     let entry_points = productions.iter().filter_map(|p| match p.vis {
         Vis::Public => Some(p.name.clone()),
         Vis::Private => None,
     });
+
+    check_all_functions_are_defined(&productions);
 
     let rt = rt::runtime_base(entry_points);
 
@@ -18,4 +23,36 @@ pub(crate) fn generate_code(productions: Vec<Production>) -> TokenStream {
         .collect();
 
     [rt, productions].into_iter().collect()
+}
+
+fn check_all_functions_are_defined(productions: &[Production]) {
+    let fn_names = productions.iter().map(|p| &p.name).collect::<HashSet<_>>();
+
+    let check_all_defined = |fns: &[Ident]| {
+        fns.iter()
+            .for_each(|fn_| 
+                // TODO: panicking here is not a good solution.
+                assert!(fn_names.get(fn_).is_some(), "Function {fn_} is not defined")
+            )
+    };
+
+    for prod in productions {
+        match &prod.kind {
+            ProductionKind::Bump { descr: _, then } | ProductionKind::CallNow { then } => {
+                check_all_defined(then)
+            }
+
+            ProductionKind::Error => {}
+
+            ProductionKind::Cond {
+                builtin: _,
+                descr: _,
+                cons,
+                alt,
+            } => {
+                check_all_defined(cons);
+                check_all_defined(alt);
+            }
+        }
+    }
 }
--- grammar-gen/src/opt.rs
+++ grammar-gen/src/opt.rs
@@ -1,5 +1,6 @@
 use crate::mir::Production;
 
 pub(crate) fn optimize_grammar(productions: Vec<Production>) -> Vec<Production> {
+    // Let's hope this function stays that way.
     productions
 }
