3146ecd732c8a406fc80a64d936e1cacd5ae034d
Sasha Pourcelot <sasha.pourcelot@protonmail.com>
2024-07-06 13:31:40 +0200

    Make the error messages in grammar-gen nicer :)
    Signed-off-by: Sasha Pourcelot <sasha.pourcelot@protonmail.com>

--- grammar-gen/src/main.rs
+++ grammar-gen/src/main.rs
@@ -9,7 +9,19 @@ mod parse;
 fn main() {
     let inpath = env::args().nth(1).expect("missing path to grammar file");
     let content = fs::read_to_string(inpath).expect("failed to read grammar file");
-    let grammar = parse::parse(&content).expect("failed to parse grammar file");
+    let grammar = match parse::parse(&content) {
+        Ok(grammar) => grammar,
+        Err(err) => {
+            panic!(
+                "failed to parse grammar file at {}:{}: {}",
+                err.span().start().line,
+                err.span().start().column,
+                err
+            );
+        }
+    };
+
+    //.expect("failed to parse grammar file");
     let productions = mir::productions_from_document(grammar);
     let productions = opt::optimize_grammar(productions);
 
