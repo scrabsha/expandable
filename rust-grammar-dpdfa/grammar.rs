25428ce5ae1853382152c0b9111585d94780e720
Sasha Pourcelot <sasha.pourcelot@protonmail.com>
2024-07-21 13:51:27 +0200

    return values after expression parsing
    Signed-off-by: Sasha Pourcelot <sasha.pourcelot@protonmail.com>

--- rust-grammar-dpdfa/grammar.rs
+++ rust-grammar-dpdfa/grammar.rs
@@ -725,6 +725,7 @@ fn expr_after_atom() {
         // https://doc.rust-lang.org/reference/expressions/operator-expr.html#arithmetic-and-logical-binary-operators
         bump();
         expr();
+        return ExprBinop;
     } else if peek(EqualsEquals)
         || peek(NotEquals)
         || peek(GreaterThan)
@@ -736,28 +737,31 @@ fn expr_after_atom() {
         // https://doc.rust-lang.org/reference/expressions/operator-expr.html#comparison-operators
         bump();
         expr();
+        return ExprBinop;
     } else if peek(OrOr) || peek(AndAnd) {
         // Lazy Boolean Expressions
         // https://spec.ferrocene.dev/expressions.html#lazy-boolean-expressions
         bump();
         expr();
+        return ExprBinop;
     } else if peek(DotDot) || peek(DotDotEquals) {
         bump();
         expr();
+        return ExprBinop;
     } else if peek(LParen) {
         expr_call();
         expr_after_atom();
+        return ExprCall;
     } else if peek(LBracket) {
         // Index expression:
         // https://doc.rust-lang.org/reference/expressions/array-expr.html#array-and-slice-indexing-expressions
         bump(LBracket);
         expr();
         bump(RBracket);
+        return ExprIndex;
     } else if peek(Dot) {
         expr_dot_expr();
         expr_after_atom();
-    } else {
-        return ExprAfterAtomEmpty;
     }
 }
 
@@ -772,30 +776,48 @@ fn expr_atom() {
         || peek(FragmentIdent)
         || peek(ColonColon)
         || peek(LessThan)
+        || peek(FragmentPath)
     {
         expr_path();
 
         if peek(Not) {
             macro_call_tail();
+        } else {
+            return ExprPath;
         }
-    } else if peek(FragmentExpr) || peek(Literal) || peek(FragmentBlock) {
+    } else if peek(Literal) {
         bump();
+        return ExprLit;
+    } else if peek(FragmentExpr) {
+        bump();
+        return ExprExprFragment;
+    } else if peek(FragmentBlock) {
+        bump();
+        return ExprBlockFragment;
     } else if peek(If) {
         expr_if();
+        return ExprIf;
     } else if peek(LParen) {
         expr_tuple();
+        return ExprTuple;
     } else if peek(LBracket) {
         expr_array();
+        return ExprArray;
     } else if peek(LBrace) {
         block();
+        return ExprBlock;
     } else if peek(Loop) || peek(FragmentLifetime) {
         expr_loop();
+        return ExprLoop;
     } else if peek(While) {
         expr_while();
+        return ExprWhile;
     } else if peek(For) {
         expr_for();
+        return ExprFor;
     } else if peek(Match) {
         expr_match();
+        return ExprMatch;
     } else {
         error();
     }
@@ -1202,15 +1224,25 @@ fn macro_call_tail() {
     bump(Not);
 
     token_stream_group();
+    if returned(TokenStreamGroupParen) {
+        return MacroCallParen;
+    } else if returned(TokenStreamGroupBracket) {
+        return MacroCallBracket;
+    } else if returned(TokenStreamGroupBrace) {
+        return MacroCallBrace;
+    }
 }
 
 fn token_stream_group() {
     if peek(LParen) {
         token_stream_group_paren();
+        return TokenStreamGroupParen;
     } else if peek(LBracket) {
         token_stream_group_bracket();
+        return TokenStreamGroupBracket;
     } else if peek(LBrace) {
         token_stream_group_brace();
+        return TokenStreamGroupBrace;
     } else {
         error();
     }
